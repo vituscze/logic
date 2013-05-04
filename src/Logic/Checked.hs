32c088e652e42db2e81599330e6486b3295c9c78
Vít Šefl <vituscze@gmail.com>
2013-05-04 19:56:30 +0200

    Added renaming.

--- src/Logic/Checked.hs
+++ src/Logic/Checked.hs
@@ -1,17 +1,29 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
 -- | Compiler checked transformation into prenex normal form.
 module Logic.Checked
     ( PrependResult(..)
     , prepend
     , remove
+    , rename
     )
-    where
+where
 
+import qualified Data.Map as Map
+
+import Control.Applicative
+import Control.Monad.Identity
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.Map (Map)
+import Data.Traversable
+
+import Data.Stream
 import Logic.Checked.Formula
 import Logic.Checked.Spec
 import Logic.PrenexTree
+import Logic.Term
 
 -- | A type of formulas produced by 'prepend'.
 --
@@ -48,3 +60,33 @@ remove (Not      f)   = mapPair Not swapAll (remove f)
 remove (And      f g) = zipPair And merge (remove f) (remove g)
 remove (Or       f g) = zipPair Or  merge (remove f) (remove g)
 remove (Implies  f g) = zipPair Implies (merge . swapAll) (remove f) (remove g)
+
+-- Renaming helpers.
+type RenameM v v' a = ReaderT (Map v v') (State (Stream v')) a
+
+localInsert :: Ord v => v -> RenameM v v' a -> RenameM v v' (Either v v', a)
+localInsert s m = do
+    s' <- state (\(x :< xs) -> (x,xs))
+    a  <- local (Map.insert s s') m
+    return (Right s', a)
+
+updateVar :: Ord v => v -> RenameM v v' (Either v v')
+updateVar v = maybe (Left v) Right . Map.lookup v <$> ask
+
+-- | Rename all bound variables to a unique name.
+rename :: Ord v => Stream v' -> Formula p t r f v
+       -> Formula p t r f (Either v v')
+rename vs f = evalState (runReaderT (go f) Map.empty) vs
+  where
+    go :: Ord v => Formula p t r f v
+       -> RenameM v v' (Formula p t r f (Either v v'))
+    go (Relation r ts) = Relation r <$> traverse goT ts
+    go (Forall x f)   = uncurry Forall <$> localInsert x (go f)
+    go (Exists x f)   = uncurry Exists <$> localInsert x (go f)
+    go (Not      f)   = Not     <$> go f
+    go (And      f g) = And     <$> go f <*> go g
+    go (Or       f g) = Or      <$> go f <*> go g
+    go (Implies  f g) = Implies <$> go f <*> go g
+
+    goT (Var x)         = Var        <$> updateVar x
+    goT (Function f ts) = Function f <$> traverse goT ts
